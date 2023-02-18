4727e1b83cb551354d52c7cb37a0d4c9f6da8d4c
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-18 08:30:56 +0100

    Add a New Character with POST

--- udemy-dotnet6-rpg/Controllers/CharacterController.cs
+++ udemy-dotnet6-rpg/Controllers/CharacterController.cs
@@ -24,5 +24,12 @@ namespace udemy_dotnet6_rpg.Controllers
 		{
 			return Ok(characters.FirstOrDefault(c => c.Id == id));
 		}
+
+		[HttpPost]
+		public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+		{
+			characters.Add(newCharacter);
+			return Ok(newCharacter);
+		}
 	}
 }
