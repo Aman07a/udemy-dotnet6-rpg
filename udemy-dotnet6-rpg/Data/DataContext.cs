eef1e309684bdcb4e476e2a61eaf3e163d92eb7e
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-18 11:08:27 +0100

    The User Model

--- udemy-dotnet6-rpg/Data/DataContext.cs
+++ udemy-dotnet6-rpg/Data/DataContext.cs
@@ -7,5 +7,6 @@ namespace udemy_dotnet6_rpg.Data
 		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
 		public DbSet<Character> Characters { get; set; }
+		public DbSet<User> Users { get; set; }
 	}
 }
