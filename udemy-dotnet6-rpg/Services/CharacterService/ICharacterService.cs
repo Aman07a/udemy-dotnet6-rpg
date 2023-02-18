98c66255946b560df3fe5ccc7fa2167bda2477e6
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-18 08:41:10 +0100

    Character Service

---
+++ udemy-dotnet6-rpg/Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,9 @@
+﻿namespace udemy_dotnet6_rpg.Services.CharacterService
+{
+	public interface ICharacterService
+	{
+		List<Character> GetAllCharacters();
+		Character GetCharacterById(int id);
+		List<Character> AddCharacter(Character newCharacter);
+	}
+}
