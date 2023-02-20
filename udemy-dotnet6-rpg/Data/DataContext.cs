eee590c07961429229d0002d1804ab416d56c00e
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-20 09:13:35 +0100

    Many-To-Many Relationship with Skills

--- udemy-dotnet6-rpg/Data/DataContext.cs
+++ udemy-dotnet6-rpg/Data/DataContext.cs
@@ -9,5 +9,6 @@ namespace udemy_dotnet6_rpg.Data
 		public DbSet<Character> Characters { get; set; }
 		public DbSet<User> Users { get; set; }
 		public DbSet<Weapon> Weapons { get; set; }
+		public DbSet<Skill> Skills => Set<Skill>();
 	}
 }
